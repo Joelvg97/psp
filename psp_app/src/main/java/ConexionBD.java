fbf46e0d1e14eda9f9125b07b37052b123a59d31
Joel Veiga <joelvg97@educastur.es>
2026-05-11 11:04:59 +0200

    Conexión a BD corregida

--- psp_app/src/main/java/ConexionBD.java
+++ psp_app/src/main/java/ConexionBD.java
@@ -6,7 +6,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/chat_denuncias";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "1111";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, PASSWORD);
